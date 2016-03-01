ed6f7a14f0d305fa87b3eafa04c61e8898abb8a0
Charles Greer <cgreer@marklogic.com>
2016-02-29 21:06:01 -0800

    Refactor testing

--- entity-services/src/test/java/com/marklogic/entityservices/TestEntityTypes.java
+++ entity-services/src/test/java/com/marklogic/entityservices/TestEntityTypes.java
@@ -4,16 +4,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -22,8 +21,6 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
@@ -33,8 +30,6 @@ import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.Difference;
 import org.custommonkey.xmlunit.XMLAssert;
 import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -45,15 +40,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hp.hpl.jena.graph.Graph;
 import com.hp.hpl.jena.sparql.graph.GraphFactory;
-import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.FailedRequestException;
-import com.marklogic.client.document.DocumentWriteSet;
-import com.marklogic.client.document.JSONDocumentManager;
 import com.marklogic.client.eval.EvalResult;
 import com.marklogic.client.eval.EvalResultIterator;
 import com.marklogic.client.eval.ServerEvaluationCall;
 import com.marklogic.client.io.DOMHandle;
-import com.marklogic.client.io.FileHandle;
 import com.marklogic.client.io.InputStreamHandle;
 import com.marklogic.client.io.JacksonHandle;
 import com.marklogic.client.io.StringHandle;
@@ -61,53 +52,6 @@ import com.marklogic.client.io.marker.AbstractReadHandle;
 
 public class TestEntityTypes extends EntityServicesTestBase {
 
-    private static DatabaseClient client;
-    private static HashMap<File, String> entityTypeUris = new HashMap<File, String>();
-    
-    private static DocumentBuilder builder;
-    
-		
-	
-    @SuppressWarnings("unchecked")
-	@BeforeClass
-    public static void setupClass() throws IOException, ParserConfigurationException {
-        TestSetup testSetup = TestSetup.getInstance();
-        client = testSetup.getClient();
-        JSONDocumentManager docMgr = client.newJSONDocumentManager();
-        DocumentWriteSet writeSet = docMgr.newWriteSet();
-        Collection<File> files = FileUtils.listFiles(new File("src/test/resources/json-entity-types"), 
-                FileFilterUtils.trueFileFilter(), FileFilterUtils.trueFileFilter());
-        Collection<File> xmlFiles = FileUtils.listFiles(new File("src/test/resources/xml-entity-types"), 
-                FileFilterUtils.trueFileFilter(), FileFilterUtils.trueFileFilter());
-        files.addAll(xmlFiles);
-        
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    	factory.setNamespaceAware(true);
-    	builder = factory.newDocumentBuilder();
-    	
-        for (File f : files) {
-        	if (f.getName().startsWith(".")) { continue; };
-        	if (! ( f.getName().endsWith(".json") || f.getName().endsWith(".xml"))) { continue; };
-        	
-        	// uncomment for quick iteration on TDE.
-        	//if (!f.getName().equals("Person-0.0.2.json")) {continue; };
-        	// if (!f.getName().equals("schema-complete-entity-type.xml")) {continue; };
-        	//if (!f.getName().startsWith("refs")) {continue; };
-        	
-        	logger.info("Loading " + f.getPath());
-        	//docMgr.write(f.getPath(), new FileHandle(f));
-            writeSet.add(f.getPath(), new FileHandle(f));
-            entityTypeUris.put(f, f.getPath());
-        }
-        docMgr.write(writeSet);
-    }
-    
-    @AfterClass
-    public static void teardownClass() {
-        // teardown.
-    }
-    
-    
     public EvalResultIterator eval(String functionCall) throws TestEvalException {
         
         String entityServicesImport = 
@@ -163,16 +107,15 @@ public class TestEntityTypes extends EntityServicesTestBase {
      * entity-type-to-xml       Serialization to XML.
      */
     public void testEntityTypeParse() throws JsonParseException, JsonMappingException, IOException, TestEvalException, SAXException, ParserConfigurationException, TransformerException {
-        for (File entityTypeFile : entityTypeUris.keySet()) {
-        	String entityTypeUri = entityTypeUris.get(entityTypeFile);
+        for (String entityType : entityTypes) {
         	ObjectMapper mapper = new ObjectMapper();
-        	logger.info("Checking "+entityTypeUri);
-        	if (entityTypeUri.contains("invalid-")) {
-        		logger.info("Checking invalid: " + entityTypeUri);
+        	logger.info("Checking "+entityType);
+        	if (entityType.toString().contains("invalid-")) {
+        		logger.info("Checking invalid: " + entityType);
         		JacksonHandle handle = null;
         		try {
-        			handle = evalOneResult("es:entity-type-from-node(fn:doc('"+ entityTypeUri  + "'))", new JacksonHandle());	
-            		fail("eval should throw an exception for invalid cases." + entityTypeUri);
+        			handle = evalOneResult("es:entity-type-from-node(fn:doc('"+ entityType.toString()  + "'))", new JacksonHandle());	
+            		fail("eval should throw an exception for invalid cases." + entityType);
         		} catch (TestEvalException e) {
         			//log.error(e.getMessage());
         			assertTrue("Must contain invalidity message", e.getMessage().contains("ES-ENTITY-TYPE-INVALID"));
@@ -184,33 +127,36 @@ public class TestEntityTypes extends EntityServicesTestBase {
         		// TDE needs enhancement.
         		// checkTriples(entityTypeUri);
 
-                if ( entityTypeFile.getName().endsWith(".json")) {
-                	JsonNode original = mapper.readValue(entityTypeFile, JsonNode.class);
-                	JacksonHandle handle  = evalOneResult("es:entity-type-from-node(fn:doc('"+ entityTypeUri  + "'))", new JacksonHandle());
+                if ( entityType.toString().endsWith(".json")) {
+                	InputStream is = this.getClass().getResourceAsStream("/json-entity-types/"+entityType);
+                	JsonNode original = mapper.readValue(is, JsonNode.class);
+                	JacksonHandle handle  = evalOneResult("es:entity-type-from-node(fn:doc('"+ entityType  + "'))", new JacksonHandle());
             		JsonNode actual = handle.get();
                     
-                    checkRoundTrip("Original node should equal serialized retrieved one: " + entityTypeFile.getName(), original, actual);
+                    checkRoundTrip("Original node should equal serialized retrieved one: " +entityType, original, actual);
                     
-                	checkEntityTypeToXML("Retrieved as XML, should match equivalent XML payload.", entityTypeFile);
+                	checkEntityTypeToXML("Retrieved as XML, should match equivalent XML payload.", entityType.toString());
                 } else {
-                	String jsonFileName = entityTypeFile.getName().replace(".xml", ".json");
-                	File jsonFile = new File("src/test/resources/json-entity-types/" + jsonFileName);
-            		
-                	JsonNode jsonEquivalent = mapper.readValue(jsonFile, JsonNode.class);
-                	JacksonHandle handle  = evalOneResult("es:entity-type-from-node(fn:doc('"+ entityTypeUri  + "'))", new JacksonHandle());
+                	String jsonFileName = entityType.toString().replace(".xml", ".json");
+                	
+                	InputStream jsonInputStreamControl = this.getClass().getResourceAsStream("/json-entity-types/" + jsonFileName);
+
+                	JsonNode jsonEquivalent = mapper.readValue(jsonInputStreamControl, JsonNode.class);
+                	JacksonHandle handle  = evalOneResult("es:entity-type-from-node(fn:doc('"+ entityType  + "'))", new JacksonHandle());
             		JsonNode jsonActual = handle.get();
                     checkRoundTrip("Converted to a map:map, the XML entity type should match the json equivalent", jsonEquivalent, jsonActual);
             		
-                	Document xmloriginal = builder.parse(entityTypeFile);
-                	DOMHandle xmlhandle  = evalOneResult("es:entity-type-to-xml(es:entity-type-from-node(fn:doc('"+ entityTypeUri  + "')))", new DOMHandle());
+                    InputStream xmlControl = this.getClass().getResourceAsStream("/xml-entity-types/"+entityType);
+                	Document xmloriginal = builder.parse(xmlControl);
+                	DOMHandle xmlhandle  = evalOneResult("es:entity-type-to-xml(es:entity-type-from-node(fn:doc('"+ entityType  + "')))", new DOMHandle());
             		Document xmlactual = xmlhandle.get();
             		
             		//debugOutput(xmloriginal);
             		//debugOutput(xmlactual);
             		
-            	    checkXMLRoundTrip("Original node should equal serialized retrieved one: " + entityTypeFile.getName(), xmloriginal, xmlactual);
+            	    checkXMLRoundTrip("Original node should equal serialized retrieved one: " + entityType, xmloriginal, xmlactual);
             	       
-            	    checkEntityTypeToJSON("Retrieved as JSON, should match equivalent JSON payload", entityTypeFile, jsonFile);
+            	    checkEntityTypeToJSON("Retrieved as JSON, should match equivalent JSON payload", entityType.toString(), jsonFileName);
             		
                 }	
         	}
@@ -228,15 +174,13 @@ public class TestEntityTypes extends EntityServicesTestBase {
     /*
      * Checks parity of XML payload when retrieved from entity type.
      */
-    private void checkEntityTypeToXML(String message, File entityTypeFile) throws TestEvalException, SAXException, IOException, ParserConfigurationException, TransformerException {
-    	String xmlFileName = entityTypeFile.getName().replace(".json", ".xml");
-    	String entityTypeUri = entityTypeUris.get(entityTypeFile);
-		File xmlFile = new File("src/test/resources/xml-entity-types/" + xmlFileName);
+    private void checkEntityTypeToXML(String message, String entityTypeFile) throws TestEvalException, SAXException, IOException, ParserConfigurationException, TransformerException {
+    	String xmlFileName = entityTypeFile.replace(".json", ".xml");
+    	InputStream xmlFile = this.getClass().getResourceAsStream("/xml-entity-types/" + xmlFileName);
 		
 		Document expectedXML = builder.parse(xmlFile);
-		String evalXML =  "es:entity-type-to-xml(es:entity-type-from-node(fn:doc('" + entityTypeUri + "')))";
+		String evalXML =  "es:entity-type-to-xml(es:entity-type-from-node(fn:doc('" + entityTypeFile + "')))";
 		
-		XMLAssert.assertXMLEqual("how bout",  "<a:x xmlns=\"\" xmlns:a=\"a\"/>",  "<a:x xmlns:a=\"a\"/>" );
 		DOMHandle handle = evalOneResult(evalXML, new DOMHandle());
 		Document actualXML = handle.get();
 		XMLUnit.setIgnoreWhitespace(true);
@@ -255,10 +199,9 @@ public class TestEntityTypes extends EntityServicesTestBase {
     /*
      * Checks parity of JSON payload when retrieved from XML-sourced entity type.
      */
-    private void checkEntityTypeToJSON(String message, File entityTypeFile, File jsonFile) throws TestEvalException {
-    	String entityTypeUri = entityTypeUris.get(entityTypeFile);
+    private void checkEntityTypeToJSON(String message, String entityTypeUri, String jsonUri) throws TestEvalException {
 		String evalJSONEqual =  "deep-equal("
-			       + "fn:doc('"+ entityTypeUris.get(jsonFile) +"')/node(), "
+			       + "fn:doc('"+ jsonUri  +"')/node(), "
                    + "es:entity-type-to-json(es:entity-type-from-node(fn:doc('" + entityTypeUri + "')))"
                    + ")";
 		

892572d9dc0de795ca53da885e69f3e853a38377
Charles Greer <cgreer@marklogic.com>
2016-03-23 11:26:04 -0700

    Added SPARQL query example

--- entity-services/src/test/java/com/marklogic/entityservices/tests/TestEntityTypeSPARQL.java
+++ entity-services/src/test/java/com/marklogic/entityservices/tests/TestEntityTypeSPARQL.java
@@ -15,6 +15,59 @@
  */
 package com.marklogic.entityservices.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.marklogic.client.io.JacksonHandle;
+import com.marklogic.client.semantics.SPARQLQueryManager;
+
 public class TestEntityTypeSPARQL extends EntityServicesTestBase {
 
+	private static SPARQLQueryManager queryMgr;
+	
+	@BeforeClass
+	public static void setup() {
+		setupClients();
+		queryMgr = client.newSPARQLQueryManager();
+	}
+	@Test
+	public void sampleSPARQLQueries() throws JsonGenerationException, JsonMappingException, IOException {
+		String docHasTypeHasPropertyHasDatatype = 
+					 "PREFIX t: <http://marklogic.com/testing-entity-type#> "
+					+"PREFIX es: <http://marklogic.com/entity-services#> "
+					+ "ASK where { t:SchemaCompleteEntityType-0.0.1 a es:EntityServicesDocument ; "
+					+ "   es:definitions ?types ."
+					+ "?types es:property ?property ."
+					+ "?property es:datatype ?datatype "
+					+ "}";
+		
+		assertTrue(queryMgr.executeAsk(queryMgr.newQueryDefinition(docHasTypeHasPropertyHasDatatype)));
+		
+		String assertTypeHasProperties =  "PREFIX t: <http://marklogic.com/testing-entity-type/SchemaCompleteEntityType-0.0.1/> "
+				+"PREFIX es: <http://marklogic.com/entity-services#> "
+				+"SELECT ?version where {"
+				+ "t:SchemaCompleteEntityType ?version \"0.0.1\" ."
+				+ "}";
+		
+		JacksonHandle handle = queryMgr.executeSelect(queryMgr.newQueryDefinition(assertTypeHasProperties), new JacksonHandle());
+		JsonNode results = handle.get();
+		
+		// to see on System.out
+		// new ObjectMapper().writerWithDefaultPrettyPrinter().writeValue(System.out, results);
+		
+		ArrayNode bindings = (ArrayNode) results.get("results").get("bindings");
+		assertEquals(1, bindings.size());
+		assertEquals("http://marklogic.com/entity-services#version", bindings.get(0).get("version").get("value").asText());
+		
+	}
 }

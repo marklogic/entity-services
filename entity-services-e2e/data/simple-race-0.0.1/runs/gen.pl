c047c6a7c8ec38c4dde6d5829ef0740561250485
sbalasub <sbalasub@marklogic.com>
2016-04-13 11:56:15 -0700

    Adding e2e folder to test end to end

---
+++ entity-services-e2e/data/simple-race-0.0.1/runs/gen.pl
@@ -0,0 +1,6 @@
+
+
+let $names := ( "Shaunda", "Bobbi", "Donette", "Arlean", "Nickole", "Tu", "Spencer", "Suzy", "Wilhelmina", "Chu", "Monica", "Evia", "Jacqualine", "Tamera", "Nova", "Bula", "Floretta", "Willow", "Reinaldo", "Nathanial", "Vasiliki", "Kenyatta", "Lenard", "Chelsie", "Dorla", "Vanna", "Rosia", "Rene", "Fermin", "Juana", "Elvina", "Rosio", "Camille", "Marhta", "Georgiana", "Rueben", "Tomasa", "Rashida", "Palmer", "Felisha", "Clarissa", "Deneen", "Tristan", "Dayle", "Cleveland", "Long", "Cierra", "Jasper", "Columbus")
+let $distances := ( 
+
+for (

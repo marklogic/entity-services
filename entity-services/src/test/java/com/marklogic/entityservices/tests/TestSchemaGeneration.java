9678caf22e9d0d240640fccc51dd8b188c589e89
Charles Greer <cgreer@marklogic.com>
2016-06-24 10:20:45 -0700

    bug:39228 unit tests for verification of duplicat handling

--- entity-services/src/test/java/com/marklogic/entityservices/tests/TestSchemaGeneration.java
+++ entity-services/src/test/java/com/marklogic/entityservices/tests/TestSchemaGeneration.java
@@ -21,6 +21,9 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.marklogic.client.document.JSONDocumentManager;
+import com.marklogic.client.io.Format;
+import com.marklogic.client.io.InputStreamHandle;
 import org.custommonkey.xmlunit.XMLAssert;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.AfterClass;
@@ -32,6 +35,10 @@ import org.xml.sax.SAXException;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.io.DOMHandle;
 import com.marklogic.client.io.StringHandle;
+
+import javax.xml.transform.TransformerException;
+
+import static org.junit.Assert.fail;
 
 /**
  * Tests the server-side function es:echema-generate($entity-type) as
@@ -51,6 +58,9 @@ public class TestSchemaGeneration extends EntityServicesTestBase {
 
 		docMgr = schemasClient.newXMLDocumentManager();
 		schemas = generateSchemas();
+		InputStream is = docMgr.getClass().getResourceAsStream("/entity-type-units/et-duplicate-prop.json");
+		JSONDocumentManager documentManager = client.newJSONDocumentManager();
+		documentManager.write("et-duplicate-prop.json", new InputStreamHandle(is).withFormat(Format.JSON));
 	}
 
 	private static void storeSchema(String entityTypeName, StringHandle schemaHandle) {
@@ -65,23 +75,29 @@ public class TestSchemaGeneration extends EntityServicesTestBase {
 		docMgr.delete(moduleName);
 	}
 
+	private static StringHandle generateSchema(String entityType) {
+		logger.info("Generating schema: " + entityType);
+		StringHandle schema = new StringHandle();
+		try {
+			schema = evalOneResult("es:entity-type-from-node( fn:doc( '" + entityType + "'))=>es:schema-generate()",
+					schema);
+		} catch (TestEvalException e) {
+			throw new RuntimeException(e);
+		}
+		return schema;
+	}
+
 	private static Map<String, StringHandle> generateSchemas() {
 		Map<String, StringHandle> map = new HashMap<String, StringHandle>();
 
 		for (String entityType : entityTypes) {
 			if (entityType.contains(".xml")) {
 				continue;
-			}
-			;
+			};
 
-			logger.info("Generating schema: " + entityType);
-			StringHandle schema = new StringHandle();
-			try {
-				schema = evalOneResult("es:entity-type-from-node( fn:doc( '" + entityType + "'))=>es:schema-generate()",
-						schema);
-			} catch (TestEvalException e) {
-				throw new RuntimeException(e);
-			}
+			StringHandle schema = generateSchema(entityType);
+
+
 			map.put(entityType, schema);
 		}
 		return map;
@@ -109,6 +125,27 @@ public class TestSchemaGeneration extends EntityServicesTestBase {
 			removeSchema(entityType);
 		}
 
+	}
+
+
+	@Test
+	public void verifyDuplicatePropertyComments() throws TestEvalException, TransformerException {
+		String entityType = "et-duplicate-prop.json";
+		StringHandle schema = generateSchema(entityType);
+		storeSchema("et-duplicate-prop.xml", schema);
+
+		String integerValidates = "<ETTwo><a>123</a></ETTwo>";
+		String stringShouldnt = "<ETTwo><a>asdf</a></ETTwo>";
+
+		DOMHandle validateResult = evalOneResult("validate strict { " + integerValidates + "}", new DOMHandle());
+        //debugOutput(validateResult.get());
+        try {
+            validateResult = evalOneResult("validate strict { " + stringShouldnt + "}", new DOMHandle());
+            fail("Should be a validation error here.");
+        } catch (TestEvalException e) {
+            //pass
+        }
+		removeSchema(entityType);
 	}
 
 	@AfterClass
